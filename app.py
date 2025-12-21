2baa16fd674269234c98c321b78760de06f60fc9
anmol sriwastava <anmol972122@gmail.com>
2025-12-22 00:28:52 +0530

    Add os import for deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -19,4 +20,5 @@ def iot():
     return render_template('iot.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)  # debug=False for production
